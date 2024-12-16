5a35697c7cd9ac2c4f979eb203d5a2a56219ea62
Adel Mohamed <adelaboras517@gmail.com>
2024-12-16 12:05:14 +0200

    first commit

---
+++ test/BookStore.EntityFrameworkCore.Tests/EntityFrameworkCore/Applications/Authors/EfCoreAuthorAppService_Tests.cs
@@ -0,0 +1,12 @@
+﻿using Acme.BookStore.Authors;
+using BookStore.EntityFrameworkCore;
+using BookStore;
+using Xunit;
+
+namespace Acme.BookStore.EntityFrameworkCore.Applications.Authors;
+
+[Collection(BookStoreTestConsts.CollectionDefinitionName)]
+public class EfCoreAuthorAppService_Tests : AuthorAppService_Tests<BookStoreEntityFrameworkCoreTestModule>
+{
+
+}
